d7c43f8cd6c340a98904a04cdf3091d1050e3e93
Mykhailo Matviichuk <michalmatviychuk@gmail.com>
2019-05-15 13:55:23 +0300

    Material paginator added and configured

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatSort, MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {PeriodicTableService} from './periodic-table.service';
 import {PeriodicElement} from './periodicElement';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-periodic-table',
@@ -10,16 +11,23 @@ import {PeriodicElement} from './periodicElement';
 })
 export class PeriodicTableComponent implements OnInit {
   title = 'open-data-front';
+  pageSizeOptions: number[];
   @ViewChild(MatSort) sort: MatSort;
   displayedColumns = ['position', 'name', 'weight', 'symbol'];
   dataSource: MatTableDataSource<PeriodicElement>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(private periodicTableService: PeriodicTableService) { }
 
   ngOnInit() {
+    this.pageSizeOptions = [5, 10, 20, 50, 100, 200, 500];
     this.periodicTableService.loadTable().subscribe(
-      (elements) => {
+      (elements: PeriodicElement[]) => {
         this.dataSource = new MatTableDataSource(elements);
         this.dataSource.sort = this.sort;
+        this.pageSizeOptions = this.pageSizeOptions.filter((len) => {
+          return len <= elements.length;
+          });
+        this.dataSource.paginator = this.paginator;
       }
     );
   }
